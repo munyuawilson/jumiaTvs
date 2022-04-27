323e8f3af3aff5a4ffe1dfcd13b3421707efb0f6
munyua <98756995+munyuawilson@users.noreply.github.com>
2022-04-27 12:18:35 +0000

    Add files via upload

--- jumia.py
+++ jumia.py
@@ -4,35 +4,50 @@ import pandas as pd
 
 
 import csv
-
-
-
-
+page=1
 list_names=[]
 list_prices=[]
-url=f"https://www.jumia.co.ke/smart-tvs-2282/?page=1#catalog-listing"
-get=requests.get(url).text
-soup=BeautifulSoup(get,'html.parser')
-for items in soup.find_all('div',class_="crs row _no-g -fw-nw _6cl-4cm -pvxs"):
-    name=items.find_all('div',class_='name')
-    price=items.find_all('div',class_='prc')
+
+
+
+while page !=49:   
+    url=f"https://www.jumia.co.ke/smart-tvs-2282/?page={page}#catalog-listing"
+
+    get=requests.get(url).text
+    soup=BeautifulSoup(get,'html.parser')
+    for items in soup.find('div',class_="-pvs col12"):
+        name=items.find_all('h3',class_='name')
+        price=items.find_all('div',class_='prc')
 
 
     for n in name:
         list_names.append(n.text)
+        
     for p in price:
         list_prices.append(p.text)
+    page+=1
 
-    
+
+range_=[]
+for r in range(len(list_names)):
+    range_.append(r)
+
+   
 dict_={
     'names':list_names,
    'prices':list_prices,
    
 }
 
-pd_data=pd.DataFrame(dict_,index=[1,2,3,4,5,6,7,])
-pd_data.to_csv('jumia.csv',index=False)
+
+
+pd_data=pd.DataFrame(dict_,index=range_)
 print(pd_data)
+pd_data.to_csv('jumia.csv',index=False)
+
+
+
+
 
 
 
